18bbafe36f3abfe75fb5793b48d702c3d0014b60
Luthfi Hadiana <luthfihadiana@gmail.com>
2021-04-29 17:13:28 +0700

    feat(add libarary): add steinhq and material icon
    add material icon CDN adn library client stein-hq

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react";
 import "./index.scss";
 import Button from "../../components/button";
+import { readAllData } from "../../api/list";
 export default function Home() {
+  const [listPrice, setListPrice] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await readAllData();
+      const filtered = data.filter(el => el.uuid);
+      setListPrice(filtered);
+    };
+    fetchData();
+  }, [setListPrice]);
   return (
     <div className="home">
       <div className="header">
         <div className="header__section">
-          <h2>List Harga Sumber Perikanan</h2>
+          <h2>List Komoditas</h2>
         </div>
         <div className="header__section">
-          <Button var="primary">Tambah Sumber Perikanan</Button>
+          <Button var="primary">Tambah Komoditas</Button>
         </div>
       </div>
-      <div className="home__content">
-        <h2>dkjasbndjks</h2>
-      </div>
+      <div className="home__content"></div>
     </div>
   );
 }
